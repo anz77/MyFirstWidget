bf5f1048df5508e72e150682864652d3a67d0c8e
Andrii Zuiok <a.zuiok77@zmail.com>
2020-12-22 21:08:02 +0200

    Initial Commit

---
+++ MyFirstWidget/MyFirstWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstWidgetApp.swift
+//  MyFirstWidget
+//
+//  Created by ANDRII ZUIOK on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
